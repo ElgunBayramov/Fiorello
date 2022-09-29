462a7b45012d46e13d07dd2637aac544e0b42429
Elguncode <elgunmb@code.edu.az>
2022-09-30 02:28:03 +0400

    Add project files.

---
+++ Fiorello/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        static List<Flower> flowers = new List<Flower>()
+        {
+            new Flower
+            {
+                FlowerId=1,
+                Name = "Messi",
+                ImageUrl="shop-8-img.jpg",
+                Price=25
+            },
+             new Flower
+            {
+                FlowerId=2,
+                Name = "ronaldo",
+                ImageUrl="shop-10-img.jpg",
+                Price=20
+            }
+        };
+       public IActionResult Index()
+        {
+            return View(flowers);
+        }
+    }
+}
---
+++ Fiorello/Models/Flower.cs
@@ -0,0 +1,13 @@
+﻿namespace Fiorello.Models
+{
+    public class Flower
+    {
+        public int FlowerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Price { get; set; }
+    }
+}
